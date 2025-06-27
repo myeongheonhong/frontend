271636f561eb8332fd21ccb6da72c587c40ea5bd
myeongheonhong <ruddyh7076@gmail.com>
2025-06-27 16:19:50 +0900

    refactor: 불필요한 컴포넌트 및 파일 삭제

--- src/components/common/Chip/type.ts
+++
@@ -1,7 +0,0 @@
-export interface ChipProps {
-  text: string;
-  chipType: 'default' | 'rounded' | 'badge' | 'info' | 'state' | 'custom';
-  className?: string;
-  color?: 'primary' | 'secondary' | 'custom';
-  variant?: 'default' | 'inverted';
-}
